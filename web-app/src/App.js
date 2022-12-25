5d9534f87c3bab2e242d46ba126f8a8fc2d7f25d
wolftossH <57511507+wolftossH@users.noreply.github.com>
2022-12-25 11:03:07 -0800

    Update App.js

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -9,14 +9,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        Hello World!!
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
